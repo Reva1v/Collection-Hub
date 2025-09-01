272880b6d9bc04e341e9598c7c93ac5c0db97d13
Reva1v <omruga06@gmail.com>
2025-09-01 16:33:31 +0300

    refactor(layout): remove AppProvider wrapper from layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import React from "react";
-import {AppProvider} from "@/contexts/AppContext";
-// import AppLayout from "@/components/AppLayout/AppLayout.tsx";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -34,9 +32,7 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-            <AppProvider>
                 {children}
-            </AppProvider>
             </body>
             </html>
     );
