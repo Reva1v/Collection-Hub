01a005ca68d63207bde31188739816dbeb2b3f76
Reva1v <omruga06@gmail.com>
2025-09-04 15:04:03 +0300

    feat(page): refactor params handling and improve data fetching for collection

--- src/app/(protected)/collections/[id]/page.tsx
+++ src/app/(protected)/collections/[id]/page.tsx
@@ -1,23 +1,29 @@
 // app/(protected)/collections/[id]/page.tsx
-import { getCollectionWithItems } from '@/lib/collections/actions'
-import { getUniqueTypes } from '@/lib/items/actions'
-import ItemsPage from '@/components/ItemsPage/ItemsPage.tsx' // можно переиспользовать компонент
+import {getCollectionWithItems} from '@/lib/collections/actions'
+import {getUniqueTypes} from '@/lib/items/actions'
+import ItemsPage from '@/components/ItemsPage/ItemsPage.tsx'
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const collectionWithItems = await getCollectionWithItems(params.id)
+interface PageProps {
+    params: Promise<{ id: string }>;
+}
+
+export default async function Page({params}: PageProps) {
+    const {id} = await params;
+
+    const collectionWithItems = await getCollectionWithItems(id);
 
     if (!collectionWithItems) {
-        return <div>Collection not found</div>
+        return <div>Collection not found</div>;
     }
 
-    const uniqueTypes = await getUniqueTypes(params.id)
+    const uniqueTypes = await getUniqueTypes(id);
 
     return (
         <ItemsPage
             collections={[collectionWithItems]}
             items={collectionWithItems.items}
-            selectedCollectionId={params.id}
+            selectedCollectionId={id}
             uniqueTypes={uniqueTypes}
         />
-    )
+    );
 }
